f67e9f0ae4389cee0f05cc9a856144c0a8a55dfa
Clinton-stack <56035975+Clinton-stack@users.noreply.github.com>
2022-06-22 17:26:56 +0200

    Add files via upload

---
+++ Java_style.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+var image1 = new Image()
+image1.src = "DSC_1129.JPG"
+var image2 = new Image()
+image2.src = "3-001.jpg"
+var image3 = new Image()
+image3.src = "14.jpg"
+var image4 = new Image()
+image4.src = "DSC_0340.JPG"
+var image5 = new Image()
+image5.src = "DSC_0238.JPG"
+var image6 = new Image()
+image6.src = "p1.jpg"
+var image7 = new Image()
+image7.src = "p4.jpg"
+
+// When the user scrolls the page, execute myFunction 
